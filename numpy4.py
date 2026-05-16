c34d185ee52f6abd75e809e2129fdb5cc0635ea8
Shah Rokan <121371680+Rokan13@users.noreply.github.com>
2026-05-17 01:42:03 +0600

    Add files via upload

---
+++ numpy4.py
@@ -0,0 +1,16 @@
+import numpy as n
+a = n.array([1,2,3,4,5])
+print(a)
+b = a.copy()
+b[1] = 10
+print(b)
+
+print(a+2)
+
+print(a**2)
+
+print(a/2)
+
+print(n.sin(a))
+print(n.cos(a))
+print(n.tan(a))
